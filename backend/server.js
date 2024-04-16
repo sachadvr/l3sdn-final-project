9ff9d68f7198293f1ed5859d04be50bf285c5b6b
theo dadon <theo.td49@gmail.com>
2024-04-16 11:47:13 +0200

    opti server.js

--- backend/server.js
+++ backend/server.js
@@ -1,22 +1,27 @@
 const express = require('express');
-const bodyParser = require('body-parser');
+require('dotenv').config();
+const app = express();
+
+// Middleware
+const authMiddleware = require('./middleware/authMiddleware');
+
+// Importation des routes
 const usersRoutes = require('./routes/users');
 const loginRoutes = require('./routes/login');
 const interviewsRoutes = require('./routes/interviews');
 const objectivesRoutes = require('./routes/objectives');
-const authMiddleware = require('./middleware/authMiddleware');
 
-const app = express();
-app.use(bodyParser.json());
+// Middleware pour l'analyse JSON
+app.use(express.json());
 
-// Routes
+// Routage
 app.use('/api/users', authMiddleware, usersRoutes);
 app.use('/api/login', loginRoutes);
 app.use('/api/interviews', authMiddleware, interviewsRoutes);
 app.use('/api/objectives', authMiddleware, objectivesRoutes);
 
-// Start server
-const port = 3000;
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+// Configuration du serveur
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Le serveur fonctionne sur le port ${PORT}`);
 });
